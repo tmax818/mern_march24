4da1415d68c581fc8b578b16474426d79055fd01
tmax818 <tylermaxwell661@gmail.com>
2024-03-05 14:12:04 -0800

    update

---
+++ lectures/02day/demos/first-react/client/src/components/Box2.jsx
@@ -0,0 +1,17 @@
+
+const Box2 = () => {
+
+    return (
+
+        <div style={{
+            backgroundColor: "purple",
+            color: "white",
+            padding: "5px",
+            margin: 5,
+            textAlign: "center"
+        }}
+        >Completed Projects</div>
+    )
+}
+
+export default Box2;
---
+++ lectures/02day/demos/first-react/client/src/components/Box3.jsx
@@ -0,0 +1,17 @@
+
+
+const Box3 = () => {
+  return (
+    <div style={{
+
+        backgroundColor: 'skyblue',
+        color: 'black',
+        padding: '5px',
+        margin: '5px',
+        textAlign: 'end',
+    }}
+    >Contact Us</div>
+  )
+}
+
+export default Box3
