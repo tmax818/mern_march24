bbb27aaa7780c97f306f7b0a35f9210c4888efb6
tmax818 <tylermaxwell661@gmail.com>
2024-03-19 07:15:59 -0700

    update pm 11

--- lectures/11day/demos/full-stack-demo/client/src/views/EditRecipe.jsx
+++ lectures/11day/demos/full-stack-demo/client/src/views/EditRecipe.jsx
@@ -5,6 +5,7 @@ import { useNavigate,useParams } from 'react-router-dom';
 const EditRecipe = () => {
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
+    const [errors, setErrors] = useState({})
     const navigate = useNavigate();
     console.log(useParams())
 
@@ -28,6 +29,10 @@ const EditRecipe = () => {
           console.log(res)
           navigate('/')
         })
+        .catch(err => {
+            console.log(err.response.data.errors)
+            setErrors(err.response.data.errors)
+        })
     
         setDescription("")
         setTitle("")
@@ -38,9 +43,11 @@ const EditRecipe = () => {
             <form onSubmit={handleSubmit}>
                 <div>
                     title: <input type="text" name="title" id="" onChange={e => setTitle(e.target.value)} value={title} />
+                    <p>{ errors.title ? errors.title.message: ""}</p>
                 </div>
                 <div>
                     description: <input type="text" name="description" id="" onChange={e => setDescription(e.target.value)} value={description} />
+                    <p>{ errors.description && errors.title.message}</p>
                 </div>
 
                 <input type="submit" value="submit" />
