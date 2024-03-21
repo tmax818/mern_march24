f66a752b20f4be30bec7f378991b9d43dd7a1f83
tmax818 <tylermaxwell661@gmail.com>
2024-03-21 07:50:08 -0700

    update

--- lectures/docs/mern-template/client/src/views/New.jsx
+++ lectures/docs/mern-template/client/src/views/New.jsx
@@ -1,7 +1,4 @@
-import {useState} from 'react';
-import {useNavigate} from 'react-router-dom';
-import { defaultThing } from '../helpers';
-import {createThing} from '../services/ThingService'
+
 import handleNew from '../hooks/handleNew';
 import Form from '../components/Form';
 
